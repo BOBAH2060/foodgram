14f291248ae43bb9118e69e6dee7a233987936e5
BOBAH2060 <159532464+BOBAH2060@users.noreply.github.com>
2026-03-04 02:45:42 +0300

    Update settings.py

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -121,7 +121,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
