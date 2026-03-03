5a354f90d30cdf4e6f0a9ed9f7df8f9003064817
BOBAH2060 <bobah2060@yandex.ru>
2026-03-03 22:54:22 +0300

    .

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,4 @@
-﻿from django.conf import settings
-
-from drf_extra_fields.fields import Base64ImageField
+﻿from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from recipes.models import (
