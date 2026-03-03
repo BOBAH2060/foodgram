31c83b56e25c901d9bcd35b836c561cbc397d3a7
BOBAH2060 <bobah2060@yandex.ru>
2026-03-03 23:31:42 +0300

    update short_link_redirect

--- backend/foodgram_backend/views.py
+++ backend/foodgram_backend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.conf import settings
 
 from api.services.short_links import RecipeShortLinkService
 
@@ -7,5 +8,8 @@ from api.services.short_links import RecipeShortLinkService
 def short_link_redirect(request, short_code):
     """Redirect a short code URL to the full recipe detail page."""
     recipe = RecipeShortLinkService.get_recipe_by_code(short_code)
-    recipe_url = reverse('recipe-detail', kwargs={'pk': recipe.pk})
-    return redirect(recipe_url)
+    relative_url = reverse('recipe-detail', kwargs={'pk': recipe.pk})
+    domain = getattr(settings, 'MEDIA_DOMAIN', None) or request.get_host()
+    absolute_url = f'{domain}{relative_url}'
+
+    return redirect(absolute_url)
